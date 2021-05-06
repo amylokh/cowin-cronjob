2f9ac24698e0823dde01c96180e4618b8bae3b26
amylokh <amylokh@gmail.com>
2021-05-06 10:39:32 +0530

    Minor tweaks

--- src/checkSlots/checkFreeSlots.js
+++ src/checkSlots/checkFreeSlots.js
@@ -5,9 +5,9 @@ const checkIfSlotsAreAvailable = (centersData) => {
     centersData.forEach((center) => {
         const { name, address, pincode, sessions } = center;
         sessions.forEach((session)=> {
-            if (session.available_capacity>0) {
+            if (session.available_capacity>0 && session.min_age_limit==45) {
                 console.log('Found a free slot at Name: '+ name + 
-                'Pincode: '+ pincode + 'Address: ' + address)
+                ' Pincode: '+ pincode + ' Address: ' + address)
                 return true;
             }
         })
