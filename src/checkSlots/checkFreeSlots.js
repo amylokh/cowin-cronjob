f34625a204f37356c189267e3cd26a23fc6281d2
amylokh <amylokh@gmail.com>
2021-05-06 10:40:52 +0530

    Change age limit to 18

--- src/checkSlots/checkFreeSlots.js
+++ src/checkSlots/checkFreeSlots.js
@@ -5,7 +5,7 @@ const checkIfSlotsAreAvailable = (centersData) => {
     centersData.forEach((center) => {
         const { name, address, pincode, sessions } = center;
         sessions.forEach((session)=> {
-            if (session.available_capacity>0 && session.min_age_limit==45) {
+            if (session.available_capacity>0 && session.min_age_limit==18) {
                 console.log('Found a free slot at Name: '+ name + 
                 ' Pincode: '+ pincode + ' Address: ' + address)
                 return true;
